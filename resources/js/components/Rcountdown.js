287a6da246c411db821de4bbf3b70798285de184
sr015 <c1118829@planet.kanazawa-it.ac.jp>
2022-08-09 21:25:05 +0900

    カウントダウン完成

--- resources/js/components/Rcountdown.js
+++ resources/js/components/Rcountdown.js
@@ -1,18 +1,18 @@
 import React , {useState} from "react";
 import { useTimer } from "react-timer-hook";
 import Mytimer from "./Mytimer";
-import Test from "./Test";
+import Countform from "./Countform";
 
 export default function Rcountdown(){
       const time = new Date();
-      const [countdownTime, setCountdownTime] = useState(100);
+      const [countdownTime, setCountdownTime] = useState(0);
       const [chengeDisplay, setChengeDisplay] = useState(true);
   time.setSeconds(time.getSeconds() + countdownTime); 
   console.log(countdownTime);
   return (
     <div> 
     <button onClick={e =>setChengeDisplay(!chengeDisplay)}>chenge</button>
-      {chengeDisplay ? ( <Mytimer expiryTimestamp={time} />): (<Test countdownTime={countdownTime} setCountdownTime={setCountdownTime}/>)} 
+      {chengeDisplay ? ( <Mytimer expiryTimestamp={time} />): (<Countform countdownTime={countdownTime} setCountdownTime={setCountdownTime}/>)} 
     </div>
   );
 }
